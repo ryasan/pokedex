e5b45bff24c002aa87370b8ca2d6f33975b7dc2c
Ryan Santos <ryansantos86@gmail.com>
2018-05-14 16:09:38 -0700

    adding app bar

--- server/index.js
+++ server/index.js
@@ -1,39 +1,22 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const livereload = require('livereload');
-// const livereloadMiddleware = require('connect-livereload');
+const fs = require('fs');
 
 const app = express();
+// const http = require('http').Server(app);
 const port = process.env.PORT || 1128;
-// const liveServer = livereload.createServer({
-//   exts: ['html', 'js'],
-//   debug: true
-// });
-// liveServer.watch(path.resolve(__dirname, './../public'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, './../public')));
-// app.use((req, res, next) => {
-//   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-//   res.setHeader('Pragma', 'no-cache');
-//   res.setHeader('Expires', '0');
-//   next();
-// });
-app.listen(port, () => {
-  console.log(`server is listening on port: ${port}`);
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
 });
 
-
-const server = livereload.createServer();
-
-server.watch(__dirname + "./../public");
-
-app.use('*', (err, res) => {
-  res.sendFile(
-    path.resolve(__dirname, './../public', 'index.html')
-  );
-});
+app.listen(port, () => console.log(`server is listening on port: ${port}`));
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render() {
-    return <div>Hi from App</div>;
+    return (
+      <div>
+        <div className="app-bar">App Title Bar</div>
+        <div className="main-content">Main Content</div>
+      </div>
+    );
   }
 }
