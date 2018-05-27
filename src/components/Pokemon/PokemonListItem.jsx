9f80db060df3eac47d43b41174b94b41d50439d6
Ryan Santos <ryansantos86@gmail.com>
2018-05-26 18:05:11 -0700

    card background color added

--- src/components/Pokemon/PokemonListItem.jsx
+++ src/components/Pokemon/PokemonListItem.jsx
@@ -3,7 +3,6 @@ import './Pokemon.scss';
 
 export default class PokemonListItem extends Component {
   render() {
-
     const { p } = this.props;
     const category = p.types[0].toLowerCase();
 
@@ -11,7 +10,7 @@ export default class PokemonListItem extends Component {
       <div className={`card ${category}`}>
         <div>{p.name}</div>
         <img src={p.imageUrl} alt={`img-${p.name}`} />
-      </div>     
+      </div>
     );
   }
 }
