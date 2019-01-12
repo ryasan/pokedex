df299d922ed88f8e0d84c86d121d813deefef184
Ryan Santos <ryansantos86@gmail.com>
2019-01-12 00:30:36 -0800

    added github link

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import brand from './../../images/pokeball.svg';
+import POKE_ICON from './../../images/pokeball.svg';
+import GH_ICON from './../../images/github.svg';
 import './AppBar.scss';
 
 const AppBar = () => {
   return (
     <div className="app-bar">
-      <div className="nav-item">
-        <img src={brand} alt="poke" />
+      <div className="poke-icon">
+        <img src={POKE_ICON} alt="poke" />
       </div>
-      <div className="nav-item title">
+      <div className="title">
         <h2>
           Poke<em>Dex</em>
         </h2>
       </div>
+      <a href="https://github.com/ryasan86/pokedex">
+        <img src={GH_ICON} alt="github" className="gh-icon" />
+      </a>
     </div>
   );
 };
