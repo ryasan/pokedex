0c5535d6c818d41eb853a4e5e26aa1dc65224488
Ryan Santos <ryansantos86@gmail.com>
2019-03-01 02:24:08 -0800

    webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,26 @@ const SRC_DIR = path.resolve(__dirname, './client/src');
 const PUB_DIR = path.resolve(__dirname, './client/public');
 
 module.exports = {
-  entry: ['babel-polyfill', SRC_DIR],
+  entry: ['babel-polyfill', SRC_DIR], // enables async await
   output: { path: PUB_DIR, filename: 'bundle.js' },
+  devServer: {
+    contentBase: PUB_DIR, // serve static files from here
+    watchContentBase: true, // initiate a page refresh if static content changes
+    proxy: [
+      // allows redirect of requests to webpack-dev-server to another destination
+      {
+        context: ['/api', '/auth'], // can have multiple
+        target: 'http://localhost:1128', // server and port to redirect to
+        secure: false
+      }
+    ],
+    port: 3000, // port webpack-dev-server listens to, defaults to 8080
+    overlay: {
+      // Shows a full-screen overlay in the browser when there are compiler errors or warnings
+      warnings: true, // default false
+      errors: true //default false
+    }
+  },
   module: {
     rules: [
       {
@@ -20,7 +38,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg|ico)$/,
         use: [
           {
-            loader: 'file-loader?name=[name].[ext]' // <-- retain original file name
+            loader: 'file-loader?name=[name].[ext]' // retain original file name
           }
         ]
       }
