00bad92bfcf90fa5f73cbf3621c67bc57a41ac83
Ryan Santos <ryansantos86@gmail.com>
2019-03-19 00:18:50 -0700

    launch browser

--- server/controllers.js
+++ server/controllers.js
@@ -2,31 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const DATA_FILE = path.join(__dirname, './data.json');
 const PER_PAGE = 12;
-
-const getPaginatedItems = (items, offset) => {
-  return items.slice(offset, offset + PER_PAGE);
-};
-
-const findOne = (pokemon, name) => {
-  return pokemon.find(p => p.name === name);
-};
-
-const filtered = (pokemon, categories, searchTerm) => {
-  return pokemon.filter(p => {
-    return categories.some(category => {
-      const searchRegex = new RegExp(searchTerm, 'gi');
-      return (
-        (p.types.includes(category) || !category) && searchRegex.test(p.name)
-      );
-    });
-  });
-};
+const { getPaginatedItems, findOne, filtered } = require('./utils');
 
 module.exports = {
   getPokemon(req, res) {
     const categories = req.query.categories.split(',');
     const searchTerm = req.query.searchTerm;
-    const offset     = req.query.offset ? parseInt(req.query.offset, 10) : 0;
+    const offset = req.query.offset ? parseInt(req.query.offset, 10) : 0;
 
     // map relevant properties from pokemon json file
     let pokemon = JSON.parse(fs.readFileSync(DATA_FILE)).map(p => ({
--- server/utils.js
+++ server/utils.js
@@ -1,18 +1,22 @@
-module.exports = {
-  getPaginatedItems(items, offset) {
-    return items.slice(offset, offset + PER_PAGE);
-  },
-  findOne(pokemon, name) {
-    return pokemon.find(p => p.name === name);
-  },
-  filtered(pokemon, categories, searchTerm) {
-    return pokemon.filter(p => {
-      return categories.some(category => {
-        const searchRegex = new RegExp(searchTerm, 'gi');
-        return (
-          (p.types.includes(category) || !category) && searchRegex.test(p.name)
-        );
-      });
-    });
-  }
+const PER_PAGE = 12;
+
+const getPaginatedItems = (items, offset) => {
+  return items.slice(offset, offset + PER_PAGE);
 };
+
+const findOne = (pokemon, name) => {
+  return pokemon.find(p => p.name === name);
+};
+
+const filtered = (pokemon, categories, searchTerm) => {
+  return pokemon.filter(p => {
+    return categories.some(category => {
+      const searchRegex = new RegExp(searchTerm, 'gi');
+      return (
+        (p.types.includes(category) || !category) && searchRegex.test(p.name)
+      );
+    });
+  });
+};
+
+module.exports = { getPaginatedItems, findOne, filtered };
